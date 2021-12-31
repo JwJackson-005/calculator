110ef508043ebfe55559c891514a55dabf00764e
Jason Jackson <jwjackson.wsu@gmail.com>
2021-12-30 22:43:16 -0800

    Calculator finished

--- javascript.js
+++ javascript.js
@@ -9,17 +9,68 @@ const negativeButton = document.querySelector('#negativeButton');
 const percentButton = document.querySelector('#percentButton');
 const decimalButton = document.querySelector('#decimalButton');
 
-function initializeButtons() {
+function initializeListeners() {
     for (let button of numberButtons) {
             button.addEventListener('click', numberClicked);
     }
     for (let button of operatorButtons) {
-        button.addEventListener('click', operatorClicked);
+        button.addEventListener('click', () => operatorClicked(button.textContent));
     }
 
     equalsButton.addEventListener('click', equalsClicked);
     clearButton.addEventListener('click', clearClicked);
+    negativeButton.addEventListener('click', negativeClicked);
+    percentButton.addEventListener('click', percentClicked);
+    decimalButton.addEventListener('click', decimalClicked);
+
+    document.addEventListener('keydown', keyPressed);
     
+}
+
+function keyPressed(e) {
+    if (e.key >= 0 && e.key <= 9) {
+        screenInput.textContent += e.key;
+    }
+    if (e.key === "Escape" || e.key === 'c') {clearClicked();}
+    if (e.key === 'Backspace') {deleteNumber()}
+    if (isOperator(e.key)) {operatorClicked(e.key)}
+    if (e.key === '=' || e.key === 'Enter') {equalsClicked();}
+    if (e.key === '.') {decimalClicked();}
+    
+}
+
+function deleteNumber() {
+    if (screenInput.textContent.length > 0) {
+        screenInput.textContent = screenInput.textContent.slice(0, -1);
+    }
+}
+
+function decimalClicked() {
+    if (!screenInput.textContent.includes('.')) {
+        screenInput.textContent += '.';
+    }
+}
+
+function percentClicked() {
+    if (screenInput.textContent !== '') {
+        screenInput.textContent = (parseFloat(screenInput.textContent) / 100).toString();
+    }
+    else if (screenExpression.textContent !== '' && 
+            !isOperator(screenExpression.textContent.charAt(screenExpression.textContent.length - 1))) {
+                screenExpression.textContent = (parseFloat(screenExpression.textContent) / 100).toString();
+    }
+}
+
+function negativeClicked() {
+    if (screenInput.textContent !== '') {
+        screenInput.textContent = (-1 * parseFloat(screenInput.textContent)).toString();
+    }
+    else if (screenExpression.textContent !== '' && 
+            !isOperator(screenExpression.textContent.charAt(screenExpression.textContent.length - 1))) {
+                console.log('here!');
+                screenExpression.textContent = (-1 * parseFloat(screenExpression.textContent)).toString();
+    }
+
 }
 
 function clearClicked(e) {
@@ -50,29 +101,35 @@ function calculate(num1, operator, num2) {
     }
 }
 
-function operatorClicked(e) {
-    let lastChar = screenExpression.textContent.charAt(screenExpression.length - 1);
-
+function operatorClicked(operator) {
+    let lastChar = screenExpression.textContent.charAt(screenExpression.textContent.length - 1);
     if (screenInput.textContent !== '') {     
-        screenExpression.textContent = screenInput.textContent + e.target.textContent;
+        screenExpression.textContent = screenInput.textContent + convert(operator);
         screenInput.textContent = '';
     }
     else {
         if (isOperator(lastChar)) {
-            screenExpression.textContent = screenExpression.textContent.slice(0, -1) + e.target.textContent;
+            screenExpression.textContent = screenExpression.textContent.slice(0, -1) + convert(operator);
         }
         else if (screenExpression.textContent !== '') {
-            screenExpression.textContent += e.target.textContent;
+            screenExpression.textContent += convert(operator);
         }
     }
 }
 
+function convert(operator) {
+    if (operator === '*') {return '×';}
+    if (operator === '/') {return '÷';}
+    if (operator === '-') {return '−';}
+    return operator;
+}
+
 function isOperator(char) {
-    return (char === '÷' || char === '×' || char === '+' || char === '−')
+    return (char === '÷' || char === '×' || char === '+' || char === '−' || char === '-' || char === '*' || char === '/')
 }
 
 function numberClicked(e) {
     screenInput.textContent += e.target.textContent;
 }
 
-initializeButtons();
+initializeListeners();
